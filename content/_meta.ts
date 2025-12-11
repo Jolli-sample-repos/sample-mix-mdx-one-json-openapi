8c6d6bf489270fcdd10025a830a871b790ca42ff
Aidan <230012360+aidancrosbie@users.noreply.github.com>
2025-12-11 10:57:10 -0800

    Update documentation (preserve custom files)

--- content/_meta.ts
+++ content/_meta.ts
@@ -7,7 +7,7 @@ export default {
   'valid-configuration-guide': 'Valid - Configuration Guide',
   'valid-getting-started': 'Valid - Getting Started',
   'api-reference': {
-    title: 'CHANGE TITLE',
+    title: 'API Reference',
     type: 'page',
     href: '/api-docs/valid-jolli-api-json'
   }
